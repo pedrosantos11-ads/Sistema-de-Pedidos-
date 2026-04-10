83ba2b0143d6fe19c4b109651c0f5d4e2b7961f4
Pedro Henrique <pedro.santos11@alunos.unis.edu.br>
2026-04-10 13:27:31 -0300

    Adicionei os objetos número e data na classe Pedido, e implementei seus devidos getters, e em seguida adicionei a function calcular total

--- Pedido.php
+++ Pedido.php
@@ -2,16 +2,47 @@
 declare(strict_types=1);
 class Pedido {
     private Cliente $cliente;
+    private int $numero;
+    private string $data;
+
     private array $itens = [];
 
-    public function __construct(Cliente $cliente) {
+    public function __construct(Cliente $cliente, int $numero, string $data) {
         $this->cliente = $cliente;
+        $this->numero = $numero;
+        $this->data = $data;
 
     }
+    public function getCliente(): Cliente {
+        return $this->cliente;
+    }
+    public function getNumero() : int {
+        return $this->numero;
+    }
+    public function getData() : string {
+        return $this->data;
+    }
+
     public function adicionarItem(ItemPedido $item): void {
     $this->itens[] = $item;
     }
     public function getItens(): array {
         return $this->itens;
     }
+
+    public function calcularTotal() : float
+    {
+        $total = 0.0;
+        foreach ($this->itens as $item) {
+            $total += $item->getProduto()->getPreco() * $item->getQuantidade();
+        }
+        return $total;
+    }
+
+
+
+
+
+
+
 }
